87cee2015bc8ab76e8f8e5746da87f8641145e07
Jayme Withers <10859571@my.uvu.edu>
2020-01-16 10:39:00 -0700

    added color options to cube

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/NewClassOne.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewClassOne : MonoBehaviour
+{
+    private Renderer _renderer;
+    private Renderer _renderer1;
+    private Renderer _renderer2;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        _renderer2 = GetComponent<Renderer>();
+        _renderer1 = GetComponent<Renderer>();
+        _renderer = GetComponent<Renderer>();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _renderer.material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            _renderer1.material.color = Color.green;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            _renderer2.material.color = Color.blue;
+        }
+    }
+}
