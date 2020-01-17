a8af6268831cc6258f861e387b3087ab39b6a06e
Jayme Withers <10859571@my.uvu.edu>
2020-01-17 12:23:45 -0700

    ForLoop tutorial

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/ForLoop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoop : MonoBehaviour
+{
+    private int numEnemies = 6;
+
+    public void Start()
+    {
+        for (int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Creating enemy number: " + i);
+        }
+    }
+}
