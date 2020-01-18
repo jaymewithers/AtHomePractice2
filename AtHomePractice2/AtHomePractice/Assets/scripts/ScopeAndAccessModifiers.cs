c923f3a7d21a19ae2b0f1eade6b1671f09f0b58f
Jayme Withers <10859571@my.uvu.edu>
2020-01-18 11:27:49 -0700

    scope and access modifiers tuturial

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/ScopeAndAccessModifiers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeAndAccessModifiers : MonoBehaviour
+{
+    public int alpha = 5;
+
+    private int beta = 0;
+    private int gamma = 5;
+
+    private AnotherClass myOtherClass;
+        
+        Void Start ()
+        {
+            Debug.Log("Alpha is set to: " + alpha);
+            alpha = 29;
+            
+            myOtherClass = new AnotherClass();
+            myOtherClass.FruitMachine(alpha, myOtherClass.apples);
+        }
+
+        void Example (int pens, int crayons)
+        {
+            int answer;
+            answer = pens * crayons * alpha;
+            Debug.Log(answer);
+        }
+}
