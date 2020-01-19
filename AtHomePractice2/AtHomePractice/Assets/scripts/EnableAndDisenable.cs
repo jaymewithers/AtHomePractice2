b80d2514b515ef76a75b4ff2dc0680a5140df391
Jayme Withers <10859571@my.uvu.edu>
2020-01-19 11:08:49 -0700

    Enabling and Disabling components tutorial

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/EnableAndDisenable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableAndDisenable : MonoBehaviour
+{
+    private Light myLight;
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+}
