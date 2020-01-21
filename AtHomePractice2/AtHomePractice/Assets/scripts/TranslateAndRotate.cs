fb1dc59d78fe3708e2b5037d43685d09db5f41aa
Jayme Withers <10859571@my.uvu.edu>
2020-01-21 11:16:32 -0700

    Translate and rotate tutorial (incomplete)

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/TranslateAndRotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class TranslateAndRotate : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+    
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward *moveSpeed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.forward *moveSpeed * Time.deltaTime);
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
