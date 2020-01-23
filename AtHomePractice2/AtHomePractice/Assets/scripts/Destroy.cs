fe0b104d98765710674f1c392130b427bddd3a86
Jayme Withers <10859571@my.uvu.edu>
2020-01-23 10:27:56 -0700

    LookAt and Destroy tutorial

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/Destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public GameObject other;
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(other, 3f);
+        }
+    }
+}
+
+// unity tutorial
---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/LookAt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform target;
+    
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+}
+
+// apply to camera then drag in object you want camera to follow to target variable field
+
+// unity tutorial
