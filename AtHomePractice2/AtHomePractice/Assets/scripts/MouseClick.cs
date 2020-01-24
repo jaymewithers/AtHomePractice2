a9068fa52998b97155762a1cd35f65234a447856
Jayme Withers <10859571@my.uvu.edu>
2020-01-24 09:13:15 -0700

    mouseclick and usingDeltatime tutorials (error in using deltatime)

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/MouseClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked on the cylinder!");
+    }
+}
+
+// unity tutorial
