bd77ca96c196ada33ee51098c5ecb3b836c84f59
Jayme Withers <10859571@my.uvu.edu>
2020-05-26 20:42:48 -0600

    on trigger enter / character mover

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/TriggerEnter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEnter : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+}
