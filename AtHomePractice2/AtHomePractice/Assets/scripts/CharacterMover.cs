1fc21228dd5e65db357ea7713fdb0b06aecbe561
Jayme Withers <10859571@my.uvu.edu>
2020-05-29 11:10:30 -0600

    scriptable object / intdata

--- AtHomePractice2/AtHomePractice/Assets/scripts/CharacterMover.cs
+++ AtHomePractice2/AtHomePractice/Assets/scripts/CharacterMover.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class CharacterMover : MonoBehaviour
 {
@@ -7,14 +8,30 @@ public class CharacterMover : MonoBehaviour
     public float speed = 10f;
     public float gravity = 3f;
     public float jumpForce = 10f;
-    
-    void Update()
+    private int jumpCount = 0;
+    public IntData jumpCountMax;
+    public UnityEvent jumpEvent;
+
+    private void Start()
     {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            positionDirection.y = 0;
+            jumpCount = 0;
+        }
+        
         positionDirection.x = Input.GetAxis("Horizontal") * speed;
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && jumpCount <= jumpCountMax.value)
         {
+            jumpEvent.Invoke();
             positionDirection.y = jumpForce;
+            jumpCount++;
         }
 
         positionDirection.y -= gravity;
---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/Scriptable Objects/IntData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+    public class IntData : ScriptableObject
+    {
+        public int value = 1;
+
+        public void ChangeValue(int number)
+        {
+            value += number;
+        }
+
+        public void SetValue(int number)
+        {
+            value = number;
+        }
+    }
