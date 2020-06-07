5b53cf643698e6ffee3e4a9968fb747afcf64904
Jayme Withers <10859571@my.uvu.edu>
2020-06-07 12:22:53 -0600

    coroutines

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/SimpleCoroutine.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SimpleCoroutine : MonoBehaviour
+{
+    public float seconds = 1f;
+    private WaitForSeconds waitObj;
+    public bool canRun { get; set; } = true;
+    public UnityEvent startEvent, repeatEvent;
+
+    private void Awake()
+    {
+        waitObj = new WaitForSeconds(seconds);
+    }
+
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+
+    public void Restart()
+    {
+        StartCoroutine(OnStartCoroutine());
+    }
+
+    private IEnumerator OnStartCoroutine()
+    {
+        canRun = true;
+        while (canRun)
+        {
+            repeatEvent.Invoke();
+            yield return waitObj;
+        }
+    }
+}
