7248d9a9beec46251b2c7e24243d220d07fba8a7
Jayme Withers <10859571@my.uvu.edu>
2020-06-27 12:35:17 -0600

    text behavior

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/Behaviors/TextBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextBehavior : MonoBehaviour
+{
+    private Text textObj;
+
+    private void Start()
+    {
+        textObj = GetComponent<Text>();
+    }
+
+    public void ChangeText(string message)
+    {
+        textObj.text = message;
+    }
+
+    public void ChangeText(IntData obj)
+    {
+        textObj.text = obj.value.ToString();
+    }
+}
