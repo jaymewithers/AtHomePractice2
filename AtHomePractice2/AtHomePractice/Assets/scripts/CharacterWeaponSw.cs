0a9b4146e8b3dd4af13b4026b27f4b737985dc7e
Jayme Withers <10859571@my.uvu.edu>
2020-06-30 10:25:09 -0600

    character weapon switch

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/CharacterWeaponSw.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterWeaponSwitch : MonoBehaviour
+{
+    public UnityEvent woodSwordEvent, metalSwordEvent, laserSwordEvent;
+    
+    public enum WeaponOptions
+    {
+        WoodSword,
+        MetalSword,
+        LaserSword
+    }
+
+    public WeaponOptions currentCharacterWeapon;
+    
+    void Update()
+    {
+        switch (currentCharacterWeapon)
+        {
+            case WeaponOptions.WoodSword:
+                woodSwordEvent.Invoke();
+                break;
+            case WeaponOptions.MetalSword:
+                metalSwordEvent.Invoke();
+                break;
+            case WeaponOptions.LaserSword:
+                laserSwordEvent.Invoke();
+                break;
+            default:
+                throw new ArgumentException();
+        }
+    }
+}
