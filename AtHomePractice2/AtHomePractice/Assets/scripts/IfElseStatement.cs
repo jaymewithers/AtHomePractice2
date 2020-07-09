f3edeb4967383b9bcd0efbca24dea5f279b7d93c
Jayme Withers <10859571@my.uvu.edu>
2020-07-09 10:35:00 -0600

    if else statement

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/IfElseStatement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class IfElseStatement : MonoBehaviour
+{
+    public int levelOne = 0, levelTwo = 1, levelThree = 2, currentLevel;
+
+    private void Start()
+    {
+        currentLevel = levelOne;
+    }
+
+
+    public void Update()
+    {
+        if (currentLevel == levelOne)
+        {
+            print("Level One");
+        }
+        else if (currentLevel == levelTwo)
+        {
+            print("Level Two");
+        }
+        else if (currentLevel == levelThree)
+        {
+            print("Level Three");
+        }
+        else
+        {
+            print("Invalid");
+        }
+    }
+}
