33e54d12e761814aa90fbc507a4f1dc111cf0b8c
Jayme Withers <10859571@my.uvu.edu>
2020-07-16 11:42:53 -0600

    mouse shooter

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/MouseShooter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseShooter : MonoBehaviour
+{
+    public GameObject ammo;
+    public UnityEvent fireEvent;
+    private Vector2 mouseLocation;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            fireEvent.Invoke();
+        }
+    }
+
+    public void Shot()
+    {
+        var ammoTransform = transform;
+        Instantiate(ammo, ammoTransform.position, ammoTransform.rotation);
+    }
+
+    public void AimAndShoot()
+    {
+        mouseLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var instance = Instantiate(ammo, transform.position, Quaternion.identity);
+        instance.transform.LookAt(mouseLocation);
+    }
+}
