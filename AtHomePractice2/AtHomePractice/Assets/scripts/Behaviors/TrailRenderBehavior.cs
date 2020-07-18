83cffdae96b9904c7470311fe21776675071c7be
Jayme Withers <10859571@my.uvu.edu>
2020-07-18 10:14:20 -0600

    trail renderer

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/Behaviors/TrailRenderBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(TrailRenderer))]
+public class TrailRenderBehavior : MonoBehaviour
+{
+    private TrailRenderer trailRenderObj;
+    public Material trailRenderMaterial;
+    
+    void Start()
+    {
+        trailRenderObj = GetComponent<TrailRenderer>();
+        trailRenderObj.time = 2f;
+        trailRenderObj.endWidth = 0.1f;
+        trailRenderObj.startWidth = 2f;
+        trailRenderMaterial.color = Color.white;
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.B))
+        {
+            trailRenderMaterial.color = Color.blue;
+        }
+
+        if (Input.GetKey(KeyCode.G))
+        {
+            trailRenderMaterial.color = Color.green;
+        }
+    }
+}
