1d96c6e3e438618c8f94fd901f6507d32f942161
Jayme Withers <10859571@my.uvu.edu>
2020-08-04 12:23:21 -0600

    list and loops

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/Scriptable Objects/ListAndLoops.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ListAndLoops : ScriptableObject
+{
+    public List<string> stringList;
+    public string singleString;
+
+    public void AddToList(string stringObj)
+    {
+        stringList.Add(stringObj);
+    }
+
+    public void RemoveFromList(string stringObj)
+    {
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            if (stringList[i] == stringObj)
+            {
+                stringList.Remove(stringObj);
+            }
+        }
+    }
+
+    public void SortList()
+    {
+        stringList.Sort();
+    }
+}
