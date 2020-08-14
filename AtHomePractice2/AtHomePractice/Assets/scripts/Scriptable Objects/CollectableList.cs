6c580245547a69d40439f321c9dfb519488e9800
Jayme Withers <10859571@my.uvu.edu>
2020-08-13 20:33:59 -0600

    collectable list (do collectable script next)

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/Scriptable Objects/CollectableList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CollectableList : ScriptableObject
+{
+  public List<Collectable> collectables;
+
+  public void AddToCollection(Collectable listObj)
+  {
+    collectables.Add(listObj);
+  }
+
+  public void RemoveFromCollection(Collectable listObj)
+  {
+    for (int i = 0; i < collectables.Count; i++)
+    {
+      collectables.Remove(listObj);
+    }
+  }
+
+  public void SortCollectables()
+  {
+    collectables.Sort();
+  }
+}
