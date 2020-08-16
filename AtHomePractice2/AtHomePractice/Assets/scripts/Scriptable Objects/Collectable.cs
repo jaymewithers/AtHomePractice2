4fa6848d7e1d2f776fb012e55ce425e98074a6f2
Jayme Withers <10859571@my.uvu.edu>
2020-08-16 10:46:57 -0600

    collectable

---
+++ AtHomePractice2/AtHomePractice/Assets/scripts/Scriptable Objects/Collectable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Collectable : ScriptableObject
+{
+    public int powerLevel = 10;
+    public Color collectableColor = Color.green;
+    public Sprite collectableSprite;
+}
